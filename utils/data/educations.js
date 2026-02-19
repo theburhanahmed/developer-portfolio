7526737f801858123768c65195db45705bf16d19
Burhan Ahmed <burhanahmed29@gmail.com>
2026-02-19 14:17:54 +0530

    Update educations.js

--- utils/data/educations.js
+++ utils/data/educations.js
@@ -2,19 +2,19 @@ export const educations = [
   {
     id: 1,
     title: "Bachelor Degree",
-    duration: "2020 - Present",
-    institution: "National University of Bangladesh",
+    duration: "2013 - 2017",
+    institution: "HKBK College of engineering",
   },
   {
     id: 2,
     title: "Higher Secondary Certificate",
-    duration: "2018 - 2020",
-    institution: "Noakhali Islamia Kamil Madrasah",
+    duration: "2011 - 2013",
+    institution: "Karnataka PU college",
   },
   {
     id: 3,
     title: "Secondary School Certificate",
-    duration: "2008 - 2018",
-    institution: "Baitus Saif Islamia Madrasah",
+    duration: "2010 - 2011",
+    institution: "Karnataka Public school",
   }
 ]
